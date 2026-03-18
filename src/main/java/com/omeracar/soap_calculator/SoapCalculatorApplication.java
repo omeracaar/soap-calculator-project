81efd9d57230ba5481672cd9cebe3c9872b1707f
Ömer Acar <omeracar34@icloud.com>
2026-03-18 14:30:28 +0300

    initial commit

---
+++ src/main/java/com/omeracar/soap_calculator/SoapCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.omeracar.soap_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omeracar/soap_calculator/SoapCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omeracar.soap_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
